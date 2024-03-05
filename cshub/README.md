dce4c1703e0715ecc5f791e32962c8c9917808a5
Wahib Ben Said <wahibbensaid@yahoo.com>
2024-03-05 17:52:08 +0000

    Updated the cshub project with Log ignored
    cshub is a side project that i started with a couple of my classmates
    Its an app that will aim to help other students who will study this course in the upcoming years

---
+++ cshub/README.md
@@ -0,0 +1,11 @@
+# cshub
+Authors:
+- Wahib Ben Said
+- Mark Harmsen
+
+
+## Errors
+Access to 127.0.0.1 was denied Error
+
+chrome://net-internals/#sockets
+[Flush socket pools]
--- cshub/app/views.py
+++ cshub/app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template,redirect,url_for,flash
-from app import app
-from app.forms import RegistrationForm, LoginForm
+from flask import render_template,redirect,url_for,flash,request
+from app import app,bcrypt,db
+from app.forms import RegistrationForm, LoginForm,UpdateAccountForm
+from app.models import User
+from flask_login import login_user,current_user, logout_user, login_required
 
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -8,19 +10,54 @@ def home():
 
 @app.route('/register',methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Registration for {form.username.data} received','success')
-        return redirect(url_for('home'))
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email= form.email.data , password = hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f"Your account has been created, you can now log in",'success')
+        return redirect(url_for('login'))
     return render_template('register.html',form=form)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login successful', 'success')
-        return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember= form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home')) #if i went to the login page while trying to access another page, this will make sure to redirect me to that page if i logged in successfully
+        else:
+            flash('Login unsuccessful, please check email and password', 'danger')
     return render_template('login.html',form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account info has been updated','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file )
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 @app.route('/Content',methods=['GET','POST'])
 def content():
@@ -37,3 +74,8 @@ def aiml():
 @app.route('/dsad',methods=['GET','POST'])
 def dsad():
     return render_template('dsad.html')
+
+@app.route('/articles',methods=['GET'])
+@login_required
+def articles():
+    return render_template('array_vs_list.html')
