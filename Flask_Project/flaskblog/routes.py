f0cd7f81d9a7bd5bac8fbf84e7365580b5d5eb4a
Wahib Ben Said <wahibbensaid@yahoo.com>
2024-03-06 10:59:20 +0000

    Create Post feature added
    Added the create post feature where users are able to create posts
    Also added the feature where each user will be able to add his own user profile picture
    Used a hex random number to store the photo name as to avoid any conflict

--- Flask_Project/flaskblog/routes.py
+++ Flask_Project/flaskblog/routes.py
@@ -1,26 +1,17 @@
+import os
+import secrets 
+from PIL import Image
 from flask import render_template,url_for,flash,redirect,request
-from flaskblog.form import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.form import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog import app,db,bcrypt
 from flaskblog.models import User, Post
 from flask_login import login_user,current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+
 @app.route('/')
 def home():
+    posts = Post.query.all()
     return render_template('home.html',posts=posts)
 
 @app.route('/about')
@@ -64,11 +55,29 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    #To resize any image to 125W, 125L we used the Pillow package so it wont take alot of space
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -79,3 +88,20 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file )
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new",  methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content=form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',form=form)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html',title=post.title, post=post)
